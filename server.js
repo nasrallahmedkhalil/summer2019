3ba3569dbfe2220a221685ae56b318c5f936f072
Ferid <helaliferid@gmail.com>
2019-08-06 17:22:09 +0100

    set cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express=require('express');
+const cors=require('cors');
 
 const app=express();
+app.use(cors());
 
 app.listen(3000,function(){
-    console.log('Server Listening on http://localhost:3000');
+    console.log('my Server Listening on http://localhost:3000');
 });
